b4948e5a06d239b5b2ae3cfe452758f9a5d53c85
Arthur <arthurlopestorres@gmail.com>
2023-07-29 20:23:52 -0300

    updating course before class

--- 12_css/js/tag.js
+++ 12_css/js/tag.js
@@ -12,14 +12,14 @@ const elements = Array.from(document.querySelectorAll('.tag'));
 
 elements.forEach(element => {
     const tagName = element.tagName.toLocaleLowerCase();
-    element.style.border = `3px solid ${colorsObj[tagName]}`
+    element.style.border = `3px solid ${ colorsObj[tagName] }`
 
-    if(!element.classList.contains('nolabel')){
-        const tag = document.createElement('span')
+    if( !element.classList.contains('nolabel') ){
+        const tag = document.createElement('span');
         tag.textContent = tagName
         tag.classList.add('elementTag')
         tag.style.color = colorsObj[tagName]
         
-        element.insertAdjacentElement('afterbegin', tag)
+        element.insertAdjacentElement('afterbegin', tag);
     }
 })
